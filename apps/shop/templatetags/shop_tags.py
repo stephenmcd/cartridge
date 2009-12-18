74d19937d739bc69fa406e17e55e94ea933fafcb
stephenmcd <steve@jupo.org>
2009-12-18 12:34:59 +1100

    thumbnail template tag

--- apps/shop/templatetags/shop_tags.py
+++ apps/shop/templatetags/shop_tags.py
@@ -1,5 +1,7 @@
 
+import os
 from django import template
+from django.conf import settings
 from shop.models import Category
 
 register = template.Library()
@@ -21,6 +23,43 @@ def money(value):
 	except:
 		value = 0.
 	return "$%.2f" % value
+
+@register.simple_tag
+def thumbnail(image_url, width=0, height=0):
+	
+	image_path = os.path.join(settings.MEDIA_ROOT, unicode(image_url))
+	try:
+		width = int(width)
+		height = int(height)
+	except ValueError:
+		return image_url
+	if not os.path.exists(image_path) or (height == 0 and width == 0):
+		return image_url
+	try:
+		from PIL import Image
+	except ImportError:
+		return image_url
+
+	image = Image.open(image_path)
+	crop = False
+	if width == 0:
+		width = image.size[0] * height / image.size[1]
+	elif height == 0:
+		height = image.size[1] * width / image.size[0]
+	else:
+		crop = True
+	if image.mode not in ("L", "RGB"):
+		image = image.convert("RGB")
+	image_dir, image_name = os.path.split(image_path)
+	thumb_name = "%s.%s.%s.jpg" % (image_name, width, height)
+	try:
+		image = image.resize((width, height), Image.ANTIALIAS)
+		if crop:
+			image = image.crop((x1, y1, x2, y2))
+		image.save(os.path.join(image_dir, thumb_name), "JPEG")
+	except:
+		return image_url
+	return "%s/%s" % (os.path.dirname(unicode(image_url)), thumb_name)
 
 def breadcrumb_menu():
 	pass
