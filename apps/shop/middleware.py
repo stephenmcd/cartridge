b9d73fd88e3d3b0d1f397cab12009a729c0bc401
stephenmcd <steve@jupo.org>
2010-01-05 00:12:48 +1100

    changed ssl middleware to use shop.settings

--- apps/shop/middleware.py
+++ apps/shop/middleware.py
@@ -1,12 +1,14 @@
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.conf import settings
+from shop.settings import SSL_ENABLED
+
 
 class SSLRedirect(object):
 	
 	def process_request(self, request):
-		if getattr(settings, "SSL_ENABLED", False):
+		if SSL_ENABLED and not getattr(settings, "DEV_SERVER", False):
 			url = "%s%s" % (request.get_host(), request.get_full_path())
 			if request.path in map(reverse, ("shop_checkout", "shop_complete")):
 				if not request.is_secure():
