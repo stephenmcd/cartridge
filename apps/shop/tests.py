0b14900ac0bf5c8bda34f62f58dde8ec4fcaa2f0
stephenmcd <steve@jupo.org>
2010-01-10 02:16:53 +1100

    remove test fixtures and add some more test placeholders

--- apps/shop/tests.py
+++ apps/shop/tests.py
@@ -2,23 +2,27 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from shop.models import Product, Category
+from shop.settings import PRODUCT_OPTIONS
 
 
 class ShopTests(TestCase):
 
 	urls = "shop.urls"
-	fixtures = ("shop_test.json",)
 
 	def test_views(self):
 		"""
 		test the main shop views for errors
 		"""
+		
+		# category
+		category = Category.objects.create(active=True)
+		response = self.client.get(category.get_absolute_url())
+		self.assertEqual(response.status_code, 200)
 
-		# category / product
-		for shop_model in (Category, Product):
-			for obj in shop_model.objects.active():
-				response = self.client.get(obj.get_absolute_url())
-				self.assertEqual(response.status_code, 200)
+		# product
+		product = Product.objects.create(active=True)
+		response = self.client.get(product.get_absolute_url())
+		self.assertEqual(response.status_code, 200)
 
 		# cart
 		response = self.client.get(reverse("shop_cart"))
@@ -28,9 +32,28 @@ class ShopTests(TestCase):
 		response = self.client.get(reverse("shop_checkout"))
 		self.assertEqual(response.status_code, 200)
 
+	def test_variations(self):
+		"""
+		test creation of variations from options and management of empty 
+		variations
+		"""
+		pass
+
+	def test_stock(self):
+		"""
+		test stock checking methods on product variations
+		"""
+		pass
+
 	def test_cart(self):
 		"""
 		test the cart
+		"""
+		pass
+
+	def test_checkout(self):
+		"""
+		test the checkout process
 		"""
 		pass
 
