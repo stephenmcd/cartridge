990380cf0174e565634b48e3aa1d1a69e583491f
stephenmcd <steve@jupo.org>
2010-01-17 20:41:25 +1100

    added cartridge site favicon

--- urls.py
+++ urls.py
@@ -1,23 +1,22 @@
 
 from django.conf.urls.defaults import patterns, url, include
-from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic.simple import direct_to_template
+
 
 admin.autodiscover()
-urlpatterns = []
 
-if settings.DEV_SERVER:
-	import os.path
-	media_url = settings.MEDIA_URL.strip("/")
-	urlpatterns += patterns("",
-		("^%s/(?P<path>.*)$" % media_url, "django.views.static.serve",
-			{"document_root": 
-			os.path.join(os.path.dirname(__file__), media_url)}),)
-
-
-urlpatterns += patterns("",
+urlpatterns = patterns("",
 	("^admin/(.*)", admin.site.root),
 	("^shop/", include("shop.urls")),
 	("^$", direct_to_template, {"template": "index.html"}),
 )
+if settings.DEV_SERVER:
+	urlpatterns += patterns("",
+		("^%s/(?P<path>.*)$" % settings.MEDIA_URL.strip("/"), 
+			"django.views.static.serve", {"document_root": settings.MEDIA_ROOT}),
+		("^favicon.ico$", "django.views.static.serve", {"document_root": 
+			settings.MEDIA_ROOT, "path": "img/favicon.ico"}),
+	)
+
