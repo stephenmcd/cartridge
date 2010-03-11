8f8477c9ad21667aabb08c19fb3193517e34bb21
stephenmcd <steve@jupo.org>
2010-03-12 07:28:54 +1100

    added admin url compatibility with django 1.0

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 
+from django import VERSION
 from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 from django.conf import settings
@@ -7,11 +8,17 @@ from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
+if VERSION[0] == 1 and VERSION[1] == 0:
+	admin_pattern = ("^admin/(.*)", admin.site.root)
+else:
+	admin_pattern = ("^admin/", include(admin.site.urls))
+
 urlpatterns = patterns("",
-    ("^admin/", include(admin.site.urls)),
+    admin_pattern,
     ("^shop/", include("shop.urls")),
     ("^$", direct_to_template, {"template": "index.html"}),
 )
+
 if settings.DEV_SERVER:
     urlpatterns += patterns("",
         ("^%s/(?P<path>.*)$" % settings.MEDIA_URL.strip("/"), 
