f778de4cb6a3594bd4b851254b2acbdb7e897cd9
stephenmcd <steve@jupo.org>
2010-03-17 12:05:21 +1100

    added logo to docs

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+#html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -195,4 +195,6 @@ latex_documents = [
 
 
 html_use_index = False
+html_theme_path = ["."]
+html_theme = "cartridge_theme"
 
