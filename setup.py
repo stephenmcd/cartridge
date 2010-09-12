e8208592baf38509664527d61346d96277ebe962
stephenmcd <steve@jupo.org>
2010-09-12 15:33:31 +1000

    Add packaging tools: setup.py and MANIFEST.

---
+++ setup.py
@@ -0,0 +1,71 @@
+
+import os
+
+exclude = ["cartridge/project_template/mezzanine.db", 
+    "cartridge/project_template/local_settings.py"]
+exclude = dict([(e, None) for e in exclude])
+for e in exclude:
+    if e.endswith(".py"):
+        try:
+            os.remove("%sc" % e)
+        except:
+            pass
+    try:
+        with open(e, "r") as f:
+            exclude[e] = (f.read(), os.stat(e))
+        os.remove(e)
+    except:
+        pass
+
+from setuptools import setup, find_packages
+
+from cartridge import __version__ as version
+from cartridge.project_template import settings
+
+try:
+    setup(
+
+        name="Cartridge",
+        version=version,
+        author="Stephen McDonald",
+        author_email="stephen.mc@gmail.com",
+        description="An Django shopping cart application.",
+        long_description=open("README.rst").read(),
+        license="BSD",
+        url="http://cartridge.jupo.org/",
+        zip_safe=False,
+        include_package_data=True,
+        packages=find_packages(),
+
+        install_requires=[
+            "setuptools",
+            "mezzanine",
+        ],
+
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Web Environment",
+            "Framework :: Django",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Internet :: WWW/HTTP",
+            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+            "Topic :: Internet :: WWW/HTTP :: WSGI",
+            "Topic :: Software Development :: Libraries :: "
+                                                "Application Frameworks",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ])
+
+finally:
+    for e in exclude:
+        if exclude[e] is not None:
+            data, stat = exclude[e]
+            try:
+                with open(e, "w") as f:
+                    f.write(data)
+                os.chown(e, stat.st_uid, stat.st_gid)
+                os.chmod(e, stat.st_mode)
+            except:
+                pass
