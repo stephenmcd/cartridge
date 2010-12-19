d119bb7c36b527596f69e0ef5cc112a8bf1a4b9e
stephenmcd <steve@jupo.org>
2010-12-20 10:58:49 +1100

    Updated urls.py in the project template to include its own url for the homepage.

--- cartridge/project_template/urls.py
+++ cartridge/project_template/urls.py
@@ -1,9 +1,18 @@
 
 from django.conf.urls.defaults import *
+from django.contrib import admin
 
-from mezzanine.project_template.urls import urlpatterns
+from mezzanine.core.views import direct_to_template
 
+
+admin.autodiscover()
+
+# Add the urlpatterns for any custom Django applications here. 
+# You can also change the ``home`` view to add your own functionality to 
+# the project's homepage.
 urlpatterns = patterns("",
+    ("^admin/", include(admin.site.urls)),
     ("^shop/", include("cartridge.shop.urls")),
-) + urlpatterns
-
+    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+    ("^", include("mezzanine.urls")),
+)
