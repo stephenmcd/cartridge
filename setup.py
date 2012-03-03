2275ed12163cd227ba2934d880c23246146a8c43
Stephen McDonald <steve@jupo.org>
2012-03-03 22:53:56 +1100

    Restore requirements.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ try:
         packages=find_packages(),
 
         install_requires=[
-#            "mezzanine >= 1.0.0",
+            "mezzanine >= 1.0.0",
             "pisa >= 3.0.33",
         ],
 
