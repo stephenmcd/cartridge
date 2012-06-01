e2c7b0f82ff6203b9342ff824f9f3a7fe6704601
Dmitry Falk <tfalc@mail.ru>
2012-06-02 01:13:09 +0400

    reorder Product/Category models code

--- cartridge/shop/models.py
+++ cartridge/shop/models.py
@@ -27,77 +27,6 @@ except ImportError:
         """
 
 
-class Category(Page, RichText):
-    """
-    A category of products on the website.
-    """
-
-    options = models.ManyToManyField("ProductOption",
-                                     verbose_name=_("Product options"),
-                                     blank=True,
-                                     related_name="product_options")
-    sale = models.ForeignKey("Sale", verbose_name=_("Sale"),
-                             blank=True, null=True)
-    price_min = fields.MoneyField(_("Minimum price"), blank=True, null=True)
-    price_max = fields.MoneyField(_("Maximum price"), blank=True, null=True)
-    combined = models.BooleanField(_("Combined"), default=True,
-        help_text=_("If checked, "
-        "products must match all specified filters, otherwise products "
-        "can match any specified filter."))
-
-    class Meta:
-        verbose_name = _("Product category")
-        verbose_name_plural = _("Product categories")
-
-    def filters(self):
-        """
-        Returns product filters as a Q object for the category.
-        """
-        # Build a list of Q objects to filter variations by.
-        filters = []
-        # Build a lookup dict of selected options for variations.
-        options = self.options.as_fields()
-        if options:
-            lookup = dict([("%s__in" % k, v) for k, v in options.items()])
-            filters.append(Q(**lookup))
-        # Q objects used against variations to ensure sale date is
-        # valid when filtering by sale, or sale price.
-        n = now()
-        valid_sale_from = Q(sale_from__isnull=True) | Q(sale_from__lte=n)
-        valid_sale_to = Q(sale_to__isnull=True) | Q(sale_to__gte=n)
-        valid_sale_date = valid_sale_from & valid_sale_to
-        # Filter by variations with the selected sale if the sale date
-        # is valid.
-        if self.sale_id:
-            filters.append(Q(sale_id=self.sale_id) & valid_sale_date)
-        # If a price range is specified, use either the unit price or
-        # a sale price if the sale date is valid.
-        if self.price_min or self.price_max:
-            prices = []
-            if self.price_min:
-                sale = Q(sale_price__gte=self.price_min) & valid_sale_date
-                prices.append(Q(unit_price__gte=self.price_min) | sale)
-            if self.price_max:
-                sale = Q(sale_price__lte=self.price_max) & valid_sale_date
-                prices.append(Q(unit_price__lte=self.price_max) | sale)
-            filters.append(reduce(iand, prices))
-        # Turn the variation filters into a product filter.
-        operator = iand if self.combined else ior
-        products = Q(id__in=self.products.only("id"))
-        if filters:
-            filters = reduce(operator, filters)
-            variations = ProductVariation.objects.filter(filters)
-            filters = [Q(variations__in=variations)]
-            # If filters exist, checking that products have been
-            # selected is neccessary as combining the variations
-            # with an empty ID list lookup and ``AND`` will always
-            # result in an empty result.
-            if self.products.count() > 0:
-                filters.append(products)
-            return reduce(operator, filters)
-        return products
-
-
 class Priced(models.Model):
     """
     Abstract model with unit and sale price fields. Inherited by
@@ -190,6 +119,77 @@ class Product(Displayable, Priced, RichText):
         return "<img src='%s%s' />" % (settings.MEDIA_URL, thumb_url)
     admin_thumb.allow_tags = True
     admin_thumb.short_description = ""
+
+
+class Category(Page, RichText):
+    """
+    A category of products on the website.
+    """
+
+    options = models.ManyToManyField("ProductOption",
+                                     verbose_name=_("Product options"),
+                                     blank=True,
+                                     related_name="product_options")
+    sale = models.ForeignKey("Sale", verbose_name=_("Sale"),
+                             blank=True, null=True)
+    price_min = fields.MoneyField(_("Minimum price"), blank=True, null=True)
+    price_max = fields.MoneyField(_("Maximum price"), blank=True, null=True)
+    combined = models.BooleanField(_("Combined"), default=True,
+        help_text=_("If checked, "
+        "products must match all specified filters, otherwise products "
+        "can match any specified filter."))
+
+    class Meta:
+        verbose_name = _("Product category")
+        verbose_name_plural = _("Product categories")
+
+    def filters(self):
+        """
+        Returns product filters as a Q object for the category.
+        """
+        # Build a list of Q objects to filter variations by.
+        filters = []
+        # Build a lookup dict of selected options for variations.
+        options = self.options.as_fields()
+        if options:
+            lookup = dict([("%s__in" % k, v) for k, v in options.items()])
+            filters.append(Q(**lookup))
+        # Q objects used against variations to ensure sale date is
+        # valid when filtering by sale, or sale price.
+        n = now()
+        valid_sale_from = Q(sale_from__isnull=True) | Q(sale_from__lte=n)
+        valid_sale_to = Q(sale_to__isnull=True) | Q(sale_to__gte=n)
+        valid_sale_date = valid_sale_from & valid_sale_to
+        # Filter by variations with the selected sale if the sale date
+        # is valid.
+        if self.sale_id:
+            filters.append(Q(sale_id=self.sale_id) & valid_sale_date)
+        # If a price range is specified, use either the unit price or
+        # a sale price if the sale date is valid.
+        if self.price_min or self.price_max:
+            prices = []
+            if self.price_min:
+                sale = Q(sale_price__gte=self.price_min) & valid_sale_date
+                prices.append(Q(unit_price__gte=self.price_min) | sale)
+            if self.price_max:
+                sale = Q(sale_price__lte=self.price_max) & valid_sale_date
+                prices.append(Q(unit_price__lte=self.price_max) | sale)
+            filters.append(reduce(iand, prices))
+        # Turn the variation filters into a product filter.
+        operator = iand if self.combined else ior
+        products = Q(id__in=self.products.only("id"))
+        if filters:
+            filters = reduce(operator, filters)
+            variations = ProductVariation.objects.filter(filters)
+            filters = [Q(variations__in=variations)]
+            # If filters exist, checking that products have been
+            # selected is neccessary as combining the variations
+            # with an empty ID list lookup and ``AND`` will always
+            # result in an empty result.
+            if self.products.count() > 0:
+                filters.append(products)
+            return reduce(operator, filters)
+        return products
 
 
 class ProductImage(Orderable):
