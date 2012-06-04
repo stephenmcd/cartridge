747986dd119db5c813d58a1b29db63516298ff6b
Stephen McDonald <steve@jupo.org>
2012-06-04 22:35:31 +1000

    Bump Mezzanine version to 1.1.1

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ try:
         packages=find_packages(),
 
         install_requires=[
-            "mezzanine >= 1.1.0",
+            "mezzanine >= 1.1.1",
             "pisa >= 3.0.33",
         ],
 
