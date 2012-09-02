b888094d61fde426e047f4c101405dcfe07c7460
Dmitry Falk <tfalc@mail.ru>
2012-09-03 02:33:32 +0400

    store wishlist in database

--- cartridge/shop/middleware.py
+++ cartridge/shop/middleware.py
@@ -1,7 +1,7 @@
 
 from mezzanine.conf import settings
 
-from cartridge.shop.models import Cart
+from cartridge.shop.models import Cart, Wishlist
 
 
 class SSLRedirect(object):
@@ -29,7 +29,13 @@ class ShopMiddleware(SSLRedirect):
     """
     def process_request(self, request):
         request.cart = Cart.objects.from_request(request)
-        wishlist = request.COOKIES.get("wishlist", "").split(",")
-        if not wishlist[0]:
+        if not request.user.is_authenticated():
+            wishlist = request.COOKIES.get("wishlist", "").split(",")
+            if not wishlist[0]:
+                wishlist = []
+        else:
+            wishlist_items = Wishlist.objects.filter(user=request.user)
             wishlist = []
+            for item in wishlist_items:
+                wishlist.append(item.sku)
         request.wishlist = wishlist
