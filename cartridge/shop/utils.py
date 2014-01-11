d9f6785fbf4b73187818bee794dd0f0e7b7317e9
Stephen McDonald <steve@jupo.org>
2014-01-11 12:37:18 +1100

    Fix for str/unicode handling of SHOP_CURRENCY_LOCALE. Closes #157.

--- cartridge/shop/utils.py
+++ cartridge/shop/utils.py
@@ -118,7 +118,7 @@ def set_locale():
     """
     Sets the locale for currency formatting.
     """
-    currency_locale = settings.SHOP_CURRENCY_LOCALE
+    currency_locale = str(settings.SHOP_CURRENCY_LOCALE)
     try:
         if setlocale(LC_MONETARY, currency_locale) == "C":
             # C locale doesn't contain a suitable value for "frac_digits".
