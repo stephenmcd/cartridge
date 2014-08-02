67c46eb27dc4c0677c48fdad23ccc9bce663210d
Stephen McDonald <steve@jupo.org>
2014-08-02 15:03:43 +1000

    Add support for testing via setuptools using Mezzanine's bits.

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ try:
         zip_safe=False,
         include_package_data=True,
         packages=find_packages(),
+        test_suite="runtests.main",
+        tests_require=["pyflakes==0.6.1", "pep8==1.4.1"],
 
         install_requires=[
             "mezzanine >= 3.1",
