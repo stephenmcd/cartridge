eb19edf116a9202a455e5d94817f3e8a284b59f9
Stephen McDonald <steve@jupo.org>
2014-11-02 12:40:54 +1100

    Define ProductAdminForm.exclude for Django 1.8.

--- cartridge/shop/forms.py
+++ cartridge/shop/forms.py
@@ -480,6 +480,7 @@ class ProductAdminForm(with_metaclass(ProductAdminFormMetaclass,
 
     class Meta:
         model = Product
+        exclude = []
 
     def __init__(self, *args, **kwargs):
         """
