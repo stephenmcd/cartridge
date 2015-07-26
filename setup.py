e98a4e483e354beffe0f9242207008d12cd4ec8b
Stephen McDonald <steve@jupo.org>
2015-07-26 13:59:14 +1000

    Typo

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ try:
             "Operating System :: OS Independent",
             "Programming Language :: Python",
             "Programming Language :: Python :: 2.7",
-            "Programming Languaqge :: Python :: 3",
+            "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.3",
             "Programming Language :: Python :: 3.4",
             "Topic :: Internet :: WWW/HTTP",
