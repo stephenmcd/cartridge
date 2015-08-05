cf1ce7397f3ac850734569d8b9a540c2a09ef7bc
Stephen McDonald <steve@jupo.org>
2015-08-06 07:49:21 +1000

    Restore settings docs

--- docs/docs_settings.py
+++ docs/docs_settings.py
@@ -1,15 +1,12 @@
 """
-This is the local_settings file for Mezzanine's docs.
+This is the local_settings file for Cartridge's docs.
 """
 
-from mezzanine.project_template.project_name.settings import *
+from cartridge.project_template.project_name.settings import *
 
-ROOT_URLCONF = "mezzanine.project_template.project_name.urls"
+ROOT_URLCONF = "cartridge.project_template.project_name.urls"
 
 # Generate a SECRET_KEY for this build
 from random import choice
 characters = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 SECRET_KEY = ''.join([choice(characters) for i in range(50)])
-
-if "mezzanine.accounts" not in INSTALLED_APPS:
-    INSTALLED_APPS = tuple(INSTALLED_APPS) + ("mezzanine.accounts",)
