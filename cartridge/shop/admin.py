385c9ccd1d5df05bacd99f6a5f3c30047900885a
David Sanders <dsanders11@ucsbalum.com>
2015-09-22 13:10:13 -0700

    Resolve css/js Media paths using static templatetag

--- cartridge/shop/admin.py
+++ cartridge/shop/admin.py
@@ -31,6 +31,7 @@ are then pushed back onto the one variation for the product.
 from copy import deepcopy
 
 from django.contrib import admin
+from django.contrib.admin.templatetags.admin_static import static
 from django.db.models import ImageField
 from django.utils.translation import ugettext_lazy as _
 
@@ -155,8 +156,8 @@ else:
 class ProductAdmin(DisplayableAdmin):
 
     class Media:
-        js = ("cartridge/js/admin/product_variations.js",)
-        css = {"all": ("cartridge/css/admin/product.css",)}
+        js = (static("cartridge/js/admin/product_variations.js"),)
+        css = {"all": (static("cartridge/css/admin/product.css"),)}
 
     list_display = product_list_display
     list_display_links = ("admin_thumb", "title")
@@ -297,7 +298,7 @@ if HAS_PDF:
 class OrderAdmin(admin.ModelAdmin):
 
     class Media:
-        css = {"all": ("cartridge/css/admin/order.css",)}
+        css = {"all": (static("cartridge/css/admin/order.css"),)}
 
     ordering = ("status", "-id")
     list_display = order_list_display
