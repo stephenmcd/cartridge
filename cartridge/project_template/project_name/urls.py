c58d33c47202c2d198bafabef3cf6955a36241d2
Stephen McDonald <steve@jupo.org>
2016-01-04 18:15:52 +1100

    Fix urlpattern changes.

--- cartridge/project_template/project_name/urls.py
+++ cartridge/project_template/project_name/urls.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.views.i18n import set_language
 from mezzanine.core.views import direct_to_template
 from mezzanine.conf import settings
 
--- cartridge/shop/urls.py
+++ cartridge/shop/urls.py
@@ -10,11 +10,12 @@ _slash = "/" if settings.APPEND_SLASH else ""
 
 urlpatterns = [
     url("^product/(?P<slug>.*)%s$" % _slash, views.product,
-    	name="shop_product"),
+        name="shop_product"),
     url("^wishlist%s$" % _slash, views.wishlist, name="shop_wishlist"),
     url("^cart%s$" % _slash, views.cart, name="shop_cart"),
     url("^checkout%s$" % _slash, views.checkout_steps, name="shop_checkout"),
-    url("^checkout/complete%s$" % _slash, views.complete, name="shop_complete"),
+    url("^checkout/complete%s$" % _slash, views.complete,
+        name="shop_complete"),
     url("^invoice/(?P<order_id>\d+)%s$" % _slash, views.invoice,
         name="shop_invoice"),
     url("^invoice/(?P<order_id>\d+)/resend%s$" % _slash,
