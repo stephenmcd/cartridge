72be0d5b90ceff3f6c490cdef1536ed58629fa56
Stephen McDonald <steve@jupo.org>
2016-01-17 16:16:29 +1100

    Update PyPI classifiers

--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ try:
             "Programming Language :: Python",
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.3",
             "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             "Topic :: Internet :: WWW/HTTP",
