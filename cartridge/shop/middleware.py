e26a1e83d30132728b1f4e7c1601937a5f5d6cd4
steve@jupo.org <steve@jupo.org>
2018-06-05 12:04:34 +1000

    Update middleware compat

--- cartridge/shop/middleware.py
+++ cartridge/shop/middleware.py
@@ -1,30 +1,12 @@
 from __future__ import unicode_literals
 
 from mezzanine.conf import settings
+from mezzanine.utils.deprecation import MiddlewareMixin
 
 from cartridge.shop.models import Cart
 
 
-class SSLRedirect(object):
-
-    def __init__(self):
-        old = ("SHOP_SSL_ENABLED", "SHOP_FORCE_HOST", "SHOP_FORCE_SSL_VIEWS")
-        for name in old:
-            try:
-                getattr(settings, name)
-            except AttributeError:
-                pass
-            else:
-                import warnings
-                warnings.warn("The settings %s are deprecated; "
-                    "use SSL_ENABLED, SSL_FORCE_HOST and "
-                    "SSL_FORCE_URL_PREFIXES, and add "
-                    "mezzanine.core.middleware.SSLRedirectMiddleware to "
-                    "MIDDLEWARE_CLASSES." % ", ".join(old))
-                break
-
-
-class ShopMiddleware(SSLRedirect):
+class ShopMiddleware(MiddlewareMixin):
     """
     Adds cart and wishlist attributes to the current request.
     """
